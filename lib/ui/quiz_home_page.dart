749d7128ce7b75c6c0c6796283b08c7cbaa0478f
Ilkhom I. Melibaev <107622192+fluttberry@users.noreply.github.com>
2025-03-25 22:07:28 +0600

    dropdownbuttons added

--- lib/ui/quiz_home_page.dart
+++ lib/ui/quiz_home_page.dart
@@ -10,6 +10,8 @@ class QuizHomePage extends StatefulWidget {
 class _QuizHomePageState extends State<QuizHomePage> {
   int count = 50;
   String v = 'Question amount';
+  // String selectedCategory = 'data1';
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -62,8 +64,9 @@ class _QuizHomePageState extends State<QuizHomePage> {
               ),
             ],
           ),
-          Slider(activeColor: Color(0xffBE52F2),
-            padding: EdgeInsets.only(left: 65, right: 65),
+          Slider(
+            activeColor: Color(0xffBE52F2),
+            padding: EdgeInsets.symmetric(horizontal: 65),
             min: 1,
             max: 50,
             label: count.toString(),
@@ -75,16 +78,68 @@ class _QuizHomePageState extends State<QuizHomePage> {
             },
           ),
           Padding(
-                padding: const EdgeInsets.only(right: 155, top:  20),
-                child: Text(
-                  'Category',
-                  style: TextStyle(
-                    fontSize: 14,
-                    fontFamily: 'SFProText',
-                    color: Colors.grey,
-                  ),
-                ),
+            padding: const EdgeInsets.only(right: 220, top: 25),
+            child: Text(
+              'Category',
+              style: TextStyle(
+                fontSize: 14,
+                fontFamily: 'SFProText',
+                color: Colors.grey,
               ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 65),
+            child: DropdownButton(dropdownColor: Colors.amber, isExpanded: true, menuMaxHeight: 30,
+              items: [
+                DropdownMenuItem(child: Text('All')),
+                // DropdownMenuItem(child: Text('data2')),
+                // DropdownMenuItem(child: Text('data3')),
+                // DropdownMenuItem(child: Text('data4')),
+                // DropdownMenuItem(child: Text('data5')),
+              ],
+                  
+              onChanged: (v) {},
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(right: 220, top: 25),
+            child: Text(
+              'Difficulty',
+              style: TextStyle(
+                fontSize: 14,
+                fontFamily: 'SFProText',
+                color: Colors.grey,
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 65),
+            child: DropdownButton(dropdownColor: Colors.amber, isExpanded: true, menuMaxHeight: 30,
+              items: [
+                DropdownMenuItem(child: Text('All')),
+                // DropdownMenuItem(child: Text('data2')),
+                // DropdownMenuItem(child: Text('data3')),
+                // DropdownMenuItem(child: Text('data4')),
+                // DropdownMenuItem(child: Text('data5')),
+              ],
+                  
+              onChanged: (v) {},
+            ),
+          ),
+          SizedBox(height: 50),
+          Container(
+            
+            height: 48, width: 200,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(5),
+              color: Color(0xff6979f8),),
+            child: InkWell(
+              
+              onTap: () {},
+            child: Text('START', style: TextStyle(fontSize: 16, fontFamily: 'SFProText'),),
+            ),
+          )
         ],
       ),
     );
