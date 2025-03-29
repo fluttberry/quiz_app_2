9a43ae3d9ffc0329e254eabd30c8c5ee46f0a543
Ilkhom I. Melibaev <107622192+fluttberry@users.noreply.github.com>
2025-03-29 21:19:47 +0600

    dropdownitems updated

--- lib/ui/home/quiz_screen.dart
+++ lib/ui/home/quiz_screen.dart
@@ -8,16 +8,15 @@ class QuizScreen extends StatefulWidget {
 }
 
 class _QuizScreenState extends State<QuizScreen> {
-int count = 50;
-String v = 'Question amount';
+  int count = 50;
+  String v = 'Question amount';
 
   @override
   Widget build(BuildContext context) {
     return Column(
-        
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Padding(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Padding(
           padding: const EdgeInsets.only(right: 300, bottom: 50, top: 0),
           child: Text(
             'Quiz',
@@ -28,70 +27,71 @@ String v = 'Question amount';
             ),
           ),
         ),
-          Center(
-            child: Padding(
-              padding: const EdgeInsets.all(30),
-              child: Container(
-                alignment: Alignment.center,
-                height: 75,
-                width: 50,
-                child: Image.asset('assets/images/AR_Tut.png'),
-              ),
-            ),
-          ),
-          SizedBox(height: 20),
-          Row(
-            // mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Padding(
-                padding: const EdgeInsets.only(left: 70, bottom: 15),
-                child: Text(
-                  'Questions amount:',
-                  style: TextStyle(
-                    fontSize: 14,
-                    fontFamily: 'SFProText',
-                    color: Colors.grey,
-                  ),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(bottom: 15),
-                child: Text(
-                  '10',
-                  style: TextStyle(fontSize: 16, fontFamily: 'SFProText'),
-                ),
-              ),
-            ],
-          ),
-          Slider(
-            activeColor: Color(0xffBE52F2),
-            padding: EdgeInsets.symmetric(horizontal: 70),
-            min: 1,
-            max: 50,
-            label: count.toString(),
-            value: count.toDouble(),
-            onChanged: (v) {
-              setState(() {
-                count = v.toInt();
-              });
-            },
-          ),
-          Padding(
-            padding: const EdgeInsets.only(right: 210, top: 25, bottom: 20),
-            child: Text(
-              'Category',
-              style: TextStyle(
-                fontSize: 14,
-                fontFamily: 'SFProText',
-                color: Colors.grey,
-              ),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 70),
+        Center(
+          child: Padding(
+            padding: const EdgeInsets.all(30),
             child: Container(
-              height: 60,
-              decoration: BoxDecoration(color: Colors.white),
+              alignment: Alignment.center,
+              height: 75,
+              width: 50,
+              child: Image.asset('assets/images/AR_Tut.png'),
+            ),
+          ),
+        ),
+        SizedBox(height: 20),
+        Row(
+          // mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(left: 70, bottom: 15),
+              child: Text(
+                'Questions amount:',
+                style: TextStyle(
+                  fontSize: 14,
+                  fontFamily: 'SFProText',
+                  color: Colors.grey,
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(bottom: 15),
+              child: Text(
+                '10',
+                style: TextStyle(fontSize: 16, fontFamily: 'SFProText'),
+              ),
+            ),
+          ],
+        ),
+        Slider(
+          activeColor: Color(0xffBE52F2),
+          padding: EdgeInsets.symmetric(horizontal: 70),
+          min: 1,
+          max: 50,
+          label: count.toString(),
+          value: count.toDouble(),
+          onChanged: (v) {
+            setState(() {
+              count = v.toInt();
+            });
+          },
+        ),
+        Padding(
+          padding: const EdgeInsets.only(right: 210, top: 25, bottom: 20),
+          child: Text(
+            'Category',
+            style: TextStyle(
+              fontSize: 14,
+              fontFamily: 'SFProText',
+              color: Colors.grey,
+            ),
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 70),
+          child: Container(
+            height: 60,
+            decoration: BoxDecoration(color: Colors.white),
+            child: DropdownButtonHideUnderline(
               child: DropdownButton(
                 elevation: 0,
                 isExpanded: true,
@@ -99,32 +99,33 @@ String v = 'Question amount';
                 iconSize: 35,
                 items: [
                   DropdownMenuItem(child: Text('  All')),
-                  // DropdownMenuItem(child: Text('data2')),
-                  // DropdownMenuItem(child: Text('data3')),
-                  // DropdownMenuItem(child: Text('data4')),
-                  // DropdownMenuItem(child: Text('data5')),
+                  DropdownMenuItem(value: 'easy', child: Text('Easy')),
+                  DropdownMenuItem(value: 'normal', child: Text('Normal')),
+                  DropdownMenuItem(value: 'hard', child: Text('Hard')),
                 ],
 
                 onChanged: (v) {},
               ),
             ),
           ),
-          Padding(
-            padding: const EdgeInsets.only(right: 210, top: 25, bottom: 20),
-            child: Text(
-              'Difficulty',
-              style: TextStyle(
-                fontSize: 14,
-                fontFamily: 'SFProText',
-                color: Colors.grey,
-              ),
+        ),
+        Padding(
+          padding: const EdgeInsets.only(right: 210, top: 25, bottom: 20),
+          child: Text(
+            'Difficulty',
+            style: TextStyle(
+              fontSize: 14,
+              fontFamily: 'SFProText',
+              color: Colors.grey,
             ),
           ),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 70),
-            child: Container(
-              height: 60,
-              decoration: BoxDecoration(color: Colors.white),
+        ),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 70),
+          child: Container(
+            height: 60,
+            decoration: BoxDecoration(color: Colors.white),
+            child: DropdownButtonHideUnderline(
               child: DropdownButton(
                 elevation: 0,
                 isExpanded: true,
@@ -133,40 +134,40 @@ String v = 'Question amount';
                 value: null,
                 items: [
                   DropdownMenuItem(child: Text('  All')),
-                  DropdownMenuItem(value: 1, child: Text('data2')),
-                  DropdownMenuItem(value: 2, child: Text('data3')),
-                  DropdownMenuItem(value: 3, child: Text('data4')),
-                  DropdownMenuItem(value: 4, child: Text('data5')),
+                  DropdownMenuItem(value: 'easy', child: Text('Easy')),
+                  DropdownMenuItem(value: 'normal', child: Text('Normal')),
+                  DropdownMenuItem(value: 'hard', child: Text('Hard')),
                 ],
 
                 onChanged: (v) {},
               ),
             ),
           ),
-          SizedBox(height: 50),
-          Container(
-            height: 48,
-            width: 200,
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(5),
-              color: Color(0xff6979f8),
-            ),
-            child: InkWell(
-              onTap: () {},
-              child: Center(
-                child: Text(
-                  'START',
-                  style: TextStyle(
-                    fontSize: 16,
-                    fontWeight: FontWeight.bold,
-                    fontFamily: 'SFProText',
-                    color: Colors.white,
-                  ),
+        ),
+        SizedBox(height: 50),
+        Container(
+          height: 48,
+          width: 200,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(5),
+            color: Color(0xff6979f8),
+          ),
+          child: InkWell(
+            onTap: () {},
+            child: Center(
+              child: Text(
+                'START',
+                style: TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold,
+                  fontFamily: 'SFProText',
+                  color: Colors.white,
                 ),
               ),
             ),
           ),
-        ],
-      );
+        ),
+      ],
+    );
   }
 }
