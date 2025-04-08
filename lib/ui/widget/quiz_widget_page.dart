47316d56761548d18a159dc75152c99842190d9e
Ilkhom I. Melibaev <107622192+fluttberry@users.noreply.github.com>
2025-04-08 20:15:28 +0600

    quiz widget page updated

--- lib/ui/widget/quiz_widget_page.dart
+++ lib/ui/widget/quiz_widget_page.dart
@@ -11,12 +11,12 @@ class QuizWidgetPage extends StatefulWidget {
 }
 
 class _QuizWidgetPageState extends State<QuizWidgetPage> {
-  int index = 0;
+  Map<int, Color> clicked = {};
   @override
   Widget build(BuildContext context) {
     return Column(
       children: [
-        SizedBox(height: 30,), 
+        SizedBox(height: 30),
         Center(
           child: Padding(
             padding: const EdgeInsets.all(35),
@@ -27,27 +27,135 @@ class _QuizWidgetPageState extends State<QuizWidgetPage> {
                 fontFamily: 'SFProText',
                 fontSize: 20,
                 fontWeight: FontWeight.w400,
-                
+
                 color: Colors.black,
               ),
             ),
           ),
         ),
-        SizedBox(height: 42,),
-        for (String answer in widget.quizResultsModel.mixedAnswers)
+        SizedBox(height: 42),
+
+        InkWell(
+          onTap: () {
+            setState(() {
+              clicked[0] =
+                  widget.quizResultsModel.mixedAnswers[0] ==
+                          widget.quizResultsModel.correctAnswer
+                      ? Colors.green
+                      : Colors.red;
+            });
+          },
+          child: Container(
+            width: 280,
+            height: 40,
+            margin: EdgeInsets.all(5),
+            decoration: BoxDecoration(
+              color: clicked[0] ?? Colors.white,
+              border: Border.all(color: Color(0xff2525ff)),
+              borderRadius: BorderRadius.circular(6),
+            ),
+            child: Center(
+              child: Text(
+                widget.quizResultsModel.mixedAnswers[0],
+                style: TextStyle(
+                  fontFamily: 'SFProText',
+                  fontSize: 14,
+                  fontWeight: FontWeight.w600,
+                  color: Color(0xff3c4dff),
+                ),
+              ),
+            ),
+          ),
+        ),
+
+        InkWell(
+          onTap: () {
+            setState(() {
+              clicked[1] =
+                  widget.quizResultsModel.mixedAnswers[1] ==
+                          widget.quizResultsModel.correctAnswer
+                      ? Colors.green
+                      : Colors.red;
+            });
+          },
+          child: Container(
+            width: 280,
+            height: 40,
+            margin: EdgeInsets.all(5),
+            decoration: BoxDecoration(
+              color: clicked[1] ?? Colors.white,
+              border: Border.all(color: Color(0xff2525ff)),
+              borderRadius: BorderRadius.circular(6),
+            ),
+            child: Center(
+              child: Text(
+                widget.quizResultsModel.mixedAnswers[1],
+                style: TextStyle(
+                  fontFamily: 'SFProText',
+                  fontSize: 14,
+                  fontWeight: FontWeight.w600,
+                  color: Color(0xff3c4dff),
+                ),
+              ),
+            ),
+          ),
+        ),
+        if (widget.quizResultsModel.type == 'multiple')
           InkWell(
-            onTap: () {},
+            onTap: () {
+              setState(() {
+                clicked[2] =
+                    widget.quizResultsModel.mixedAnswers[2] ==
+                            widget.quizResultsModel.correctAnswer
+                        ? Colors.green
+                        : Colors.red;
+              });
+            },
             child: Container(
               width: 280,
               height: 40,
               margin: EdgeInsets.all(5),
               decoration: BoxDecoration(
+                color: clicked[2] ?? Colors.white,
                 border: Border.all(color: Color(0xff2525ff)),
                 borderRadius: BorderRadius.circular(6),
               ),
               child: Center(
                 child: Text(
-                  answer,
+                  widget.quizResultsModel.mixedAnswers[2],
+                  style: TextStyle(
+                    fontFamily: 'SFProText',
+                    fontSize: 14,
+                    fontWeight: FontWeight.w600,
+                    color: Color(0xff3c4dff),
+                  ),
+                ),
+              ),
+            ),
+          ),
+        if (widget.quizResultsModel.type == 'multiple')
+          InkWell(
+            onTap: () {
+              setState(() {
+                clicked[3] =
+                    widget.quizResultsModel.mixedAnswers[3] ==
+                            widget.quizResultsModel.correctAnswer
+                        ? Colors.green
+                        : Colors.red;
+              });
+            },
+            child: Container(
+              width: 280,
+              height: 40,
+              margin: EdgeInsets.all(5),
+              decoration: BoxDecoration(
+                color: clicked[3] ?? Colors.white,
+                border: Border.all(color: Color(0xff2525ff)),
+                borderRadius: BorderRadius.circular(6),
+              ),
+              child: Center(
+                child: Text(
+                  widget.quizResultsModel.mixedAnswers[3],
                   style: TextStyle(
                     fontFamily: 'SFProText',
                     fontSize: 14,
