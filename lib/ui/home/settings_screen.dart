2a99ce4b97e7315cfeef4549a8bd68e999602d7a
Ilkhom I. Melibaev <107622192+fluttberry@users.noreply.github.com>
2025-04-13 23:50:07 +0600

    settings creen is almost done

--- lib/ui/home/settings_screen.dart
+++ lib/ui/home/settings_screen.dart
@@ -5,6 +5,119 @@ class SettingsScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Text('Settings');
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        const Padding(
+          padding: EdgeInsets.all(20),
+          child: Text(
+            'Settings',
+
+            style: TextStyle(
+              fontSize: 24,
+              fontFamily: 'SFProText',
+              fontWeight: FontWeight.w700,
+            ),
+          ),
+        ),
+        SizedBox(height: 20,),
+        Container(
+          decoration: BoxDecoration(border: Border.all(color: Color(0xffEEEEEE),),),
+          height: 90,
+          width: double.infinity,
+          child: Padding(
+            padding: const EdgeInsets.all(25),
+            child: Row(
+              
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  'Share this app',
+                  style: TextStyle(
+                    fontSize: 18,
+                    fontFamily: 'SFProText',
+                    fontWeight: FontWeight.w400,
+                    color: Colors.black
+                  ),
+                ),
+                Icon(Icons.arrow_forward_ios, size: 15, color: Color(0xffbe52f2)),
+              ],
+            ),
+          ),
+        ),
+        Container(
+          decoration: BoxDecoration(border: Border.all(color: Color(0xffEEEEEE),),),
+          height: 90,
+          width: double.infinity,
+          child: Padding(
+            padding: const EdgeInsets.all(25),
+            child: Row(
+              
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  'Rate us',
+                  style: TextStyle(
+                    fontSize: 18,
+                    fontFamily: 'SFProText',
+                    fontWeight: FontWeight.w400,
+                    color: Colors.black
+                  ),
+                ),
+                Icon(Icons.arrow_forward_ios, size: 15, color: Color(0xffbe52f2)),
+              ],
+            ),
+          ),
+        ),
+        Container(
+          decoration: BoxDecoration(border: Border.all(color: Color(0xffEEEEEE),),),
+          height: 90,
+          width: double.infinity,
+          child: Padding(
+            padding: const EdgeInsets.all(25),
+            child: Row(
+              
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  'Leave fedback',
+                  style: TextStyle(
+                    fontSize: 18,
+                    fontFamily: 'SFProText',
+                    fontWeight: FontWeight.w400,
+                    color: Colors.black
+                  ),
+                ),
+                Icon(Icons.arrow_forward_ios, size: 15, color: Color(0xffbe52f2)),
+              ],
+            ),
+          ),
+        ),
+        Container(
+          decoration: BoxDecoration(border: Border.all(color: Color(0xffEEEEEE),),),
+          height: 90,
+          width: double.infinity,
+          child: Padding(
+            padding: const EdgeInsets.all(25),
+            child: Row(
+              
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  'Clear history',
+                  style: TextStyle(
+                    fontSize: 18,
+                    fontFamily: 'SFProText',
+                    fontWeight: FontWeight.w400,
+                    color: Colors.black
+                  ),
+                ),
+                Icon(Icons.arrow_forward_ios, size: 15, color: Color(0xffbe52f2)),
+              ],
+            ),
+          ),
+        ),
+      ],
+    );
   }
 }
